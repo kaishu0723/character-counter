49df7ecd4bbe47168af41459f567c3a212fe0a9a
kaishu <m.kaishu0723@gmailcom>
2025-02-25 14:12:00 +0900

    fixed pdf.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Count from "./components/count";
-// import Pdf from "./components/pdf";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center m-10">Character Counter</h1>
       <Count />
-      {/* <Pdf /> */}
     </div>
   );
 }
