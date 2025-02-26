a7d262a92fdbf733f7968298f5d3b96176f4b453
kaishu <m.kaishu0723@gmailcom>
2025-02-26 23:13:06 +0900

    add shadow at checkbox

--- src/app/components/count.tsx
+++ src/app/components/count.tsx
@@ -41,8 +41,11 @@ const Count = () => {
         <div>
             <p className="text-center mt-10">Character count: {isChecked ? text.length : trimText.length}</p>
             <label className="flex p-4 justify-center">
-                <div id="container" className={`w-10 h-5 ${isChecked ? 'bg-green-300' : 'bg-gray-300'} rounded-full relative cursor-pointer mx-2`} onClick={() => setIsChecked(!isChecked)}>
-                    <div id="toggle" className={`w-5 h-5 bg-white rounded-full absolute left-0 top-0 transition-all duration-300 ${isChecked ? 'left-5' : 'left-0'}`}></div></div>
+                <div id="container" className={`w-10 h-5 ${isChecked ? 'bg-green-300' : 'bg-gray-300'} rounded-full shadow-inner shadow-black/10 relative cursor-pointer mx-2`} onClick={() => setIsChecked(!isChecked)}>
+                    <div id="toggle" className={`w-5 h-5 bg-white rounded-full absolute left-0 top-0 transition-all duration-300 ${isChecked ? 'left-5' : 'left-0'}`}>
+                        {/* {isChecked ? <ContentCopyIcon /> : <AutoAwesomeIcon />} */}
+                    </div>
+                </div>
                 <p>スペース、改行、タブをカウントする</p>
             </label>
             <textarea
@@ -60,9 +63,9 @@ const Count = () => {
                     <AutoAwesomeIcon />
                     <button onClick={handleClear} id="clear-button" className="pl-2">clear</button>
                 </div>
-                
+
                 <div className="block w-[130px] mx-auto m-10 bg-white text-blue-500 border-2 border-blue-500 p-2 rounded-md text-center">
-                    <BackspaceIcon className="relative bottom-[1px]"/>
+                    <BackspaceIcon className="relative bottom-[1px]" />
                     <button onClick={handleRemove} id="clear-button" className="pl-2">Remove</button>
                 </div>
             </div>
